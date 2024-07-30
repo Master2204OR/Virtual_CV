3f69ec597129d9030118352a0507a797b1e53d9a
Master2204OR <108833716+Master2204OR@users.noreply.github.com>
2024-07-30 12:11:14 +0200

    Navigation bar done
    I have completed my final design of the navigation bar. I have also deleted some files that I was using to practice with.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
 // scripts.js
+
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const scrollToTopButton = document.getElementById('scrollToTop');
 
